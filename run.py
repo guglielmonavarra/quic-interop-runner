8f822b4cb30dcb684c1f761b7ed6d2b7bf3409b3
Guglielmo <navarra.guglielmo@gmail.com>
2023-05-29 21:41:38 +0200

    Add own images that manage CC algo

--- run.py
+++ run.py
@@ -64,7 +64,7 @@ def parse_args():
         "--ccalgo",
         nargs="+",
         default="bbr",
-        choices=["reno", "cubic", "bbr", "fast"],
+        choices=["reno", "cubic", "bbr", "bbr2", "fast"],
         help="congestion algo.",
     )
     parser.add_argument(
